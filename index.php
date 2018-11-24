2b588d4736f3246b4c3cb152d71b5e07e12f85a7
guinhomoraes <guilhermeguilherme98@gmail.com>
2018-11-24 11:20:41 -0200

    inicio

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php require_once 'header.php'; ?>
+</head>
+<body>
+	<?php require_once 'topo.php'; ?>
+	<?php require_once 'banner.php'; ?>
+	<?php require_once 'carrossel_produtos.php'; ?>
+	<section class="mb-80">
+	  <div class="container">
+	    <div class="row">
+	     <div class="col-lg-12 col-md-12">
+	      <div class="action-box gray-bg">
+	        <div class="action-box-text">
+	         <h3><strong> Webster: </strong> The most powerful template ever on the market</h3>
+	         <p>Create tailor-cut websites with the exclusive multi-purpose responsive template along with powerful features.</p>
+	         </div>
+	         <div class="action-box-button">
+	        <a class="button button-border black" href="#">
+	          <span>Purchase Now</span>
+	          <i class="fa fa-download"></i>
+	       </a> 
+	       </div>
+	    </div>
+	    </div>
+	  </div>
+	  </div>
+	</section>
+	<?php require_once 'nossos_servicos.php'; ?>
+	
+	<?php require_once 'fornecedores.php'; ?>
+	<?php require_once 'rodape.php'; ?>
+</body>
+<?php require_once 'footer.php'; ?>
+</html>
